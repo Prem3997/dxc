200a8b4321b41892cb3afa559a8800e74af3f7f3
Prem3997 <37274588+Prem3997@users.noreply.github.com>
2019-09-20 09:12:27 +0530

    Add files via upload

---
+++ Day3/phonenumber.js
@@ -0,0 +1,10 @@
+<script type="text/javascript">
+    function validation()
+    {
+    pwd= document.getElementById("phonenumber").value
+    if (pwd.length==0){
+        alert("Phone Number should not be empty")
+        return false
+    }
+}
+</script>
